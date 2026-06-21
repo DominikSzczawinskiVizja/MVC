aed22e3f49ed2a69bf38a99c658f527fca8091e5
DominikSzcawinskiVizja <szczado9917_aehit@students.vizja.pl>
2026-06-21 12:11:44 +0200

    Dodanie DotNet

---
+++ DotNet/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MvcMovie.Data
+{
+
+    public class MvcMovieContext(DbContextOptions<MvcMovieContext> options) : DbContext(options)
+    {
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+
+    }
+}
